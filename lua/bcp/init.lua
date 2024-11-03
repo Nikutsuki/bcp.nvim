9d911346d19974ec10e49cc2eb264c48b41928a0
Matthew <nikutsuki@icloud.com>
2024-11-03 23:32:17 +0100

    Load copilot instantly to avoid errors

--- lua/bcp/init.lua
+++ lua/bcp/init.lua
@@ -24,6 +24,7 @@ M.get_client = function()
 end
 
 M.setup = function(opts)
+	local copilot = require("copilot")
 	local blink_cmp_config = require("blink.cmp.config")
 	blink_cmp_config.kind_icons["Copilot"] = ""
 	M.get_client()
